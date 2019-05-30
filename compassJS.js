1ef428a8734c3fc2e3c1687b096c781d9a25ea50
James Hunt <37090809+James-J-Hunt@users.noreply.github.com>
2019-05-30 16:33:12 +1200

    Testing a Few things
    Testing the difference between the location calculations, and the watch position changes

--- compassJS.js
+++ compassJS.js
@@ -1,9 +1,20 @@
-// Version 7
+// Version 8
 
 // Compass Code and alpha data etc inspired and adapted from HTML5 for the Mobile Web: Device Orientation Events
 // https://mobiforge.com/design-development/html5-mobile-web-device-orientation-events
 
 // Code also an combination of many helpful tutorials online but no major code taken just used to fix small issues
+
+// Testing and Works!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// - Galaxy S9
+
+
+// c1 location = -41.29632743891323, 174.7798232221797
+// c2 location = -41.29668717414107, 174.78073323191197
+// d location  = -41.2967531,        174.7806263
+// diverge              4                   4
+// 5th decimal place
+
 
 function compass () {
   // Declares variables now so they can be used by everything
@@ -51,6 +62,10 @@ function compass () {
 
       // Watches the users current Pos and returns the values to be used by the code below
       navigator.geolocation.watchPosition(function(position) {
+        enableHighAccuracy: true;
+        timeout:6000;
+        maximumAge: 0;
+
         latC = position.coords.latitude; // Finds the Current Latitude
         longC = position.coords.longitude; // Finds the Current Longitude
 
@@ -75,16 +90,18 @@ function compass () {
       if (angle < 180) {direction = angle;}
       else {direction = (360 - angle) * -1;}
 
-      //Displaying of the data
-
+      //Displaying of the data as a compass
       nArrow.style.Transform = 'rotate(' + alpha + 'deg)';
       nArrow.style.WebkitTransform = 'rotate('+ webkitAlpha + 'deg)';
-      //Rotation is reversed for FF
       nArrow.style.MozTransform = 'rotate(-' + alpha + 'deg)'; 
 
       dArrow.style.transform = 'rotate(' + direction + 'deg)';
       dArrow.style.WebkitTransform = 'rotate(' + direction + 'deg)';
       dArrow.style.MozTransform = 'rotate(' + direction + 'deg)';
+
+      // Calculates the prixoimity to the location
+      latA = latC - latD;
+      longA = longC - longD;
 
       // TESTING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       document.getElementById('latC').innerHTML = latC;
@@ -94,6 +111,8 @@ function compass () {
       document.getElementById('heading').innerHTML = heading;
       document.getElementById('alpha').innerHTML = alpha;
       document.getElementById('direction').innerHTML = direction;
+      document.getElementById('latA').innerHTML = latA;
+      document.getElementById('longA').innerHTML = longA;
       // TESTING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     }, false); // This could also be what loops the code. I am not fully sure
